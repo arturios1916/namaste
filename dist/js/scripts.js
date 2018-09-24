2a6285484f4ca59d2e742b158991f13d2d302288
arturios1916 <arturios1996@gmail.com>
2018-09-24 19:58:41 +0300

    add pug

---
+++ dist/js/scripts.js
@@ -0,0 +1,2 @@
+"use strict";$(document).ready(function(){$(".owl-carousel").owlCarousel({dots:!0,items:4})});
+//# sourceMappingURL=scripts.js.map
---
+++ src/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        dots: true,
+        items: 4,
+
+    });
+  });
