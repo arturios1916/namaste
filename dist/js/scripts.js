5fe8f9e65392ad0993a49710841ac7b32d0de914
arturios1916 <arturios1996@gmail.com>
2018-09-26 19:54:28 +0300

    pixel perfect

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,2 +1,2 @@
-"use strict";$(document).ready(function(){$(".owl-carousel").owlCarousel({dots:!0,items:4}),$(".head__subcaption").arctext({radius:250})});
+"use strict";$(document).ready(function(){$(".owl-carousel").owlCarousel({dots:!0,items:4}),$(".head__subcaption").arctext({radius:200})});
 //# sourceMappingURL=scripts.js.map
