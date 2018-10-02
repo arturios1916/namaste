ba37d3f956b4d010c7135cc0fdda9809bd51ebca
arturios1916 <arturios1996@gmail.com>
2018-10-02 18:11:45 +0300

    fix IE10 bug, add js scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,9 +2,62 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         dots: true,
         items: 4,
+        responsive : {
+            0 : {
+                items: 2,
+            },
+            576 : {
+                items: 2,
+            },
+            768 : {
+                items: 4,
+                dots: true
+            }
+        }
     
     });
+    //radius text(plugin)
     $(".head__subcaption").arctext({
         radius: 200,
     })
   });
+
+//blocks animation
+Revealator.effects_padding  =  '-400' ;
+
+// browser detect
+var doc = document.getElementById('js-head');
+doc.setAttribute('data-useragent', navigator.userAgent);
+
+class Scroll {
+    constructor (el) {
+        this._el = el;
+        this._scrollEl;
+        this._temp;
+        this._coord;
+        this.Init();
+    }
+    Init(){
+        const self = this;
+        $(self._el).on('click', '.nav__link',function (){
+            self._temp = $(this).attr('scroll');
+            self._scrollEl = self._temp;
+            self.ElementCord();
+            console.log(self._temp)
+        });
+    }
+    ElementCord(){
+        const self = this;
+        var section = document.getElementById(self._scrollEl);
+        var coord = section.getBoundingClientRect().top;
+        self._coord = coord;
+        self.Scroll();
+    }
+    Scroll(){
+        const self = this;
+        $('html, body').animate({
+            scrollTop: self._coord + "px"
+        }, 700, 'swing');
+    }
+}
+const scroll = new Scroll('.js-scroll');
